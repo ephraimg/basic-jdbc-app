490a29dc80caeefead0db0593add7bd0932a50be
Ephraim Glick <eglick@ripple.com>
2023-07-14 13:27:13 -0700

    Connect to DB with JDBC using automatic service discovery

--- src/main/java/com/ephraimglick/basicjdbcapp/BasicJdbcApp.java
+++ src/main/java/com/ephraimglick/basicjdbcapp/BasicJdbcApp.java
@@ -3,14 +3,47 @@ package com.ephraimglick.basicjdbcapp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.*;
 
 public class BasicJdbcApp {
     public static void main(String[] args) throws IOException {
+        String DB_URL = "jdbc:postgresql://localhost/";
+        String USER = "postgres";
+        String PASS = "password";
+
+        try {
+            // Driver is registered via automatic service discovery (possible in JDBC 4+).
+            // This relies on the driver being on the classpath.
+            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            initDb(conn);
+            saveDataFromUser(conn);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void initDb(Connection conn) throws SQLException {
+        Statement stmt = conn.createStatement();
+        String sql = "" +
+                "CREATE SCHEMA IF NOT EXISTS music;" +
+                "CREATE TABLE IF NOT EXISTS music.artists (artist_name VARCHAR(25) PRIMARY KEY, rating smallint);";
+        stmt.executeUpdate(sql);
+        System.out.println("Database set up successfully...");
+    }
+
+    private static void saveDataFromUser(Connection conn) throws IOException, SQLException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
         System.out.println("Enter an artist");
         String name = reader.readLine();
         System.out.println("Enter a rating (0 - 4)");
-        String rating = reader.readLine();
-        System.out.println(name + ": " + rating + " stars");
+        int rating = Integer.parseInt(reader.readLine());
+
+        String query = "INSERT INTO music.artists (artist_name, rating) values (?, ?);";
+        PreparedStatement stmt = conn.prepareStatement(query);
+        stmt.setString(1, name);
+        stmt.setInt(2, rating);
+        stmt.execute();
+        System.out.println("Saved artist " + name + " with rating " + rating);
     }
 }
